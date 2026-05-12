d57bd39c674c7ddb8a8a6291cb212f1aeb1a6c93
prem-web.dev <a70064182@gmail.com>
2026-05-13 02:28:59 +0530

    Update BoardArea.tsx

--- src/components/Board/BoardArea.tsx
+++ src/components/Board/BoardArea.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect, useState } from 'react';
+import React, { useRef, useState } from 'react';
 import { useBoardStore } from '../../store/useBoardStore';
 import { StickyNote } from './StickyNote';
 import { motion, AnimatePresence } from 'framer-motion';
